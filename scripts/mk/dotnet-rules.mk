a7c1f1a2384a48f5ceb93f727b4b09debab85461
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2026-02-20 20:55:00 +0100

    build(make): add initial build and test rules
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

---
+++ scripts/mk/dotnet-rules.mk
@@ -0,0 +1,15 @@
+
+
+.PHONY: build
+build:  ## Build the application
+	dotnet build
+
+.PHONY: test
+test:  ## Run the tests
+	dotnet test
+
+# .PHONY: deps
+# deps:
+	# dotnet add package Microsoft.
+
+
